fc802f9e34786b6602cff4f7fb5671b2a4f0e30f
xuqingkai <id@xuqingkai.com>
2022-07-22 18:00:05 +0800

    list数组初始化。。

--- List.dart
+++ List.dart
@@ -1,16 +1,17 @@
 void main() {
+  //指定长度
   List list1 = List<String>.filled(5, '');
-  List list2= List<int>.filled(5, 0);
+  List list2 = List<int>.filled(5, 0);
   List list3 = List.filled(5, '');
 
+  //动态数组，初始化
+  List list0 = List.from({"1111", "2222"});
+  //动态数组，后续添加
   List list = [];
-  list.addAll(["a"]);
-  list.add("b");
+  list.addAll(["a", "b"]);
+  list.add("c");
+  list.remove("b");
   list.forEach((element) {
     print(element);
   });
-
-
-
-
 }
